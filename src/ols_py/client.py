3366a561431f9064900966ed02ee0f77e09619e7
Marius <marius.mather@sydney.edu.au>
2022-09-19 17:00:03 +1000

    Rework URL creation

--- src/ols_py/client.py
+++ src/ols_py/client.py
@@ -1,6 +1,6 @@
 from typing import Optional, Type, TypeVar
+from urllib.parse import quote_plus
 
-import furl
 import pydantic
 import requests
 
@@ -14,25 +14,44 @@ class OlsClient:
     Client for communicating with an OLS instance.
     """
 
+    base_url: str
+
     def __init__(self, base_url: str):
         """
         :param base_url: Base API URL for the OLS instance
         """
-        self.base_url = furl.furl(base_url)
+        if not base_url.endswith("/"):
+            base_url = base_url + "/"
+        self.base_url = base_url
         self._session = requests.Session()
         self._session.headers = {"accept": "application/json"}
         # TODO: do we need to set access-control-allow-origin header?
+
+    def _create_url(self, path: str) -> str:
+        # Remove leading /
+        path = path.lstrip("/")
+        return self.base_url + path
+
+    @staticmethod
+    def _quote_iri(iri: str) -> str:
+        """
+        Quote an IRI as a double URL-encoded URL string, so it can
+        be used in a URL path
+        :param iri: IRI, e.g. http://purl.obolibrary.org/obo/GO_0043226
+        :return: Percent-encoded string, e.g. http%253A%252F%252Fpurl.obolibrary.org%252Fobo%252FGO_0043226
+        """
+        return quote_plus(quote_plus(iri))
 
     def get(self, path: str, params: Optional[dict] = None) -> dict:
         """
         Perform a GET request to the API.
 
         :param path: API path (excluding base url)
-        :param params: Query parameters
+        :param params: Query parametersgg
         :return: JSON data, as a dict
         """
-        url = self.base_url / path
-        resp = self._session.get(url=url.url, params=params)
+        url = self._create_url(path)
+        resp = self._session.get(url=url, params=params)
         resp.raise_for_status()
         return resp.json()
 
