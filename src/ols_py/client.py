2044496f24fd3e14205f5a7199a25d1b02706ce6
Marius <marius.mather@sydney.edu.au>
2022-09-19 13:35:50 +1000

    Docstring for OlsClient.get()

--- src/ols_py/client.py
+++ src/ols_py/client.py
@@ -8,13 +8,27 @@ S = TypeVar("S", bound=pydantic.BaseModel, covariant=True)
 
 
 class OlsClient:
+    """
+    Client for communicating with an OLS instance.
+    """
+
     def __init__(self, base_url: str):
+        """
+        :param base_url: Base API URL for the OLS instance
+        """
         self.base_url = furl.furl(base_url)
         self._session = requests.Session()
         self._session.headers = {"accept": "application/json"}
         # TODO: do we need to set access-control-allow-origin header?
 
-    def get(self, path: str, params: Optional[dict] = None):
+    def get(self, path: str, params: Optional[dict] = None) -> dict:
+        """
+        Perform a GET request to the API.
+
+        :param path: API path (excluding base url)
+        :param params: Query parameters
+        :return: JSON data, as a dict
+        """
         url = self.base_url / path
         resp = self._session.get(url=url.url, params=params)
         resp.raise_for_status()
