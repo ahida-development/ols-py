f7f1e65990307afa458d7d0041518207aec28618
Marius <marius.mather@sydney.edu.au>
2022-10-05 17:03:20 +1100

    Start documenting individual fields in schemas

--- docs/schemas.md
+++ docs/schemas.md
@@ -3,12 +3,17 @@
 We validate request parameters and responses
 with [pydantic](https://pydantic-docs.helpmanual.io)
 to try and provide more certainty about the
-data sent and received. This should also
+data sent and received from the OLS API. This should also
 mean you get useful type annotations when working
 with the client in your own code
 (you may want to install a Pydantic plugin for
 your IDE/editor, e.g. [VS Code](https://pydantic-docs.helpmanual.io/visual_studio_code/)).
 
+
 ## :::ols_py.schemas.requests
+    options:
+      show_bases: false
 
 ## :::ols_py.schemas.responses
+    options:
+      show_bases: false
--- src/ols_py/schemas/requests.py
+++ src/ols_py/schemas/requests.py
@@ -14,7 +14,9 @@ class PageParams(BaseModel):
     """
 
     size: Optional[PositiveInt]
+    """Number of results per page"""
     page: Optional[NonNegativeInt]
+    """Which page to fetch (starting at 0)"""
 
 
 SearchReturnFields = Literal[
@@ -50,19 +52,38 @@ class SearchParams(BaseModel):
     """
 
     q: str
+    """Query to search for"""
     ontology: Optional[list[str]]
+    """Ontologies to search, e.g. `["mondo", "upheno"]`"""
     type: Optional[EntityType]
+    """Type of term to search for, e.g. "class", "property" """
     slim: Optional[list[str]]
     fieldList: Optional[list[SearchReturnFields]]
+    """Which fields to return in the results"""
     queryFields: Optional[list[SearchQueryFields]]
+    """Which fields to search over"""
     exact: Optional[bool]
     groupField: Optional[bool]
+    """Group results by unique ID"""
     obsoletes: Optional[bool]
+    """Include obsoleted terms in the results"""
     local: Optional[bool]
+    """Only return terms in a defining ontology"""
     childrenOf: Optional[list[str]]
+    """Restrict results to children of these terms"""
     allChildrenOf: Optional[list[str]]
+    """
+    Restrict results to children of these terms, plus other
+    child-like relations e.g. "part of", "develops from"
+    """
     rows: Optional[int]
+    """
+    Number of results per page
+    """
     start: Optional[int]
+    """
+    Index of first result
+    """
 
     @validator(
         "ontology",
